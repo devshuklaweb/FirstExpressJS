6ddca133e0c8ebeb682fd1b193e3fdaa11bdb08c
Devendra Shukla <devshukla0123@gmail.com>
2024-05-26 13:04:40 +0530

    add.use code commit

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const path = require("path"); //its required for getting the __dirname
 const app = express()
 const port = 3000
 
+//serve my public folder directory
+app.use(express.static(path.join(__dirname,"public")))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/about', (req, res) => {
-    // res.sendFile(path.join(__dirname,'index.html')); //for showing html page
+    res.sendFile(path.join(__dirname,'index.html')); //for showing html page
     // res.status(200);//for sending status
-    res.json({name:"devendra",age:"shukla"});
+   //res.json({name:"devendra",age:"shukla"});
 })
 
 app.get('/contact', (req, res) => {
