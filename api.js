23a2c2a7b5bb62682c7f90b5c772735bd134c876
Devendra Shukla <devshukla0123@gmail.com>
2024-06-08 18:30:57 +0530

    delete and list api created in mongoose

--- api.js
+++ api.js
@@ -9,12 +9,22 @@ const app = express();
 const port = 3000
 
 app.use(express.json());
+//create api
 app.post('/create', async(req,resp)=>{
     let data = new Comments(req.body);
     let result = await data.save();
     resp.send(result);
 })
-
+//list api
+app.get('/list', async(req,resp)=>{
+    let data = await Comments.find();
+    resp.send(data);
+})
+//delete api
+app.delete('/del/:id', async(req,resp)=>{
+    let data = await Comments.deleteOne({_id:req.params.id});
+    resp.send(data);
+})
 app.listen(port, () => {
   console.log(`Blogs app listening on port http://localhost:${port}`)
 })
