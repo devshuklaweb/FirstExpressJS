797273b8c040c0e1e75fc940b71f990582319885
Devendra Shukla <devshukla0123@gmail.com>
2024-06-09 16:36:23 +0530

    files upload

--- uploadFile.js
+++ uploadFile.js
@@ -5,18 +5,18 @@ const port = 3000
 
 //middleware for upload file
 const uploadFilesMiddleware = multer({
-    storage:multer.diskStorage({
-        destination: function(req,file,callback) {
-            callback(null,"uploads");//uploads is a folder for uploading
+    storage: multer.diskStorage({
+        destination: function (req, file, callback) {
+            callback(null, "uploads");//uploads is a folder for uploading
         },
-        filename:function(req,file,callback) {
-            callback(null,file.fieldname+"_"+Date.now()+".jpg");
-        }   
+        filename: function (req, file, callback) {
+            callback(null, file.fieldname + "_" + Date.now() + ".jpg");
+        }
     })
 }).single("upload_photo");//for uploading single image and postman fieldname: imageFields
-app.post('/uploadMyFile', uploadFilesMiddleware, async(req,resp)=>{
+app.post('/uploadMyFile', uploadFilesMiddleware, async (req, resp) => {
     resp.send("Files uploaded successfully");
 })
 app.listen(port, () => {
     console.log(`Blogs app listening on port http://localhost:${port}`)
-  })
+})
