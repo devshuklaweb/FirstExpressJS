0d5e8250537a7d79edf88d22af54c6afeb88ab38
Devendra Shukla <devshukla0123@gmail.com>
2024-06-09 16:11:28 +0530

    commit code for single file upload in node

---
+++ uploadFile.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const multer = require("multer")
+const app = express();
+const port = 3000
+
+//middleware for upload file
+const uploadFilesMiddleware = multer({
+    storage:multer.diskStorage({
+        destination: function(req,file,callback) {
+            callback(null,"uploads");//uploads is a folder for uploading
+        },
+        filename:function(req,file,callback) {
+            callback(null,file.fieldname+"_"+Date.now()+".jpg");
+        }   
+    })
+}).single("upload_photo");//for uploading single image and postman fieldname: imageFields
+app.post('/uploadMyFile', uploadFilesMiddleware, async(req,resp)=>{
+    resp.send("Files uploaded successfully");
+})
+app.listen(port, () => {
+    console.log(`Blogs app listening on port http://localhost:${port}`)
+  })
